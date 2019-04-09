287b0cb533472ffa61e85721e311e01133ecd807
pushnoy100500 <romanenko.igor2014@outlook.com>
2019-04-08 23:05:03 -0400

    dz 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,31 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
+function hasNestedDirs(pathname) {
+  const dirs = pathname.split('/');
+  return dirs.length > 1;
+}
+
+function serveFile(filepath, res) {
+  const fileStream = fs.createReadStream(filepath);
+
+  fileStream.on('error', (err) => {
+    switch (err.code) {
+      case 'ENOENT':
+        res.statusCode = 404;
+        res.end('requested file does not exist');
+        break;
+      default:
+        res.statusCode = 500;
+        res.end('server error occured');
+        break;
+    }
+  })
+
+  fileStream.pipe(res);
+}
 
 const server = new http.Server();
 
@@ -11,9 +36,12 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-
+      if (hasNestedDirs(pathname)) {
+        res.statusCode = 400;
+        res.end('nested directories are not supported');
+      }
+      serveFile(filepath, res);
       break;
-
     default:
       res.statusCode = 501;
       res.end('Not implemented');
